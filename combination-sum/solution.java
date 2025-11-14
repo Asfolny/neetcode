c5a4569c1f62478cd8954d4cc1256a37de0ce692
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-11-14 22:58:47 +0100

    JAVA: ALTERNATE solution for Subsets
    Uses a loop internally, tho this is usually worse than countdown

--- combination-sum/solution.java
+++ combination-sum/solution.java
@@ -1,26 +1,28 @@
 class Solution {
     List<List<Integer>> res;
     public List<List<Integer>> combinationSum(int[] nums, int target) {
-        res = new ArrayList<List<Integer>>();
-        var set = new ArrayList<Integer>();
-        dfs(nums, 0, target, set);
+        res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        dfs(nums, 0, target, 0, new ArrayList<>());
         return res;
     }
 
-    private void dfs(int[] nums, int index, int target, List<Integer> cur) {
-        if (target == 0) {
-            res.add(new ArrayList(cur));
+    private void dfs(int[] nums, int index, int target, int total, List<Integer> cur) {
+        if (total == target) {
+            res.add(new ArrayList<>(cur));
             return;
         }
 
-        if (target < 0 || index >= nums.length) {
-            return;
-        }
+        for (var j = index; j < nums.length; j++) {
+            if (total + nums[j] > target) {
+                return;
+            }
 
-        cur.add(nums[index]);
-        dfs(nums, index, target - nums[index], cur);
-        cur.remove(cur.size()-1);
-        dfs(nums, index+1, target, cur);
+            cur.add(nums[j]);
+            dfs(nums, j, target, total + nums[j], cur);
+            cur.remove(cur.size() - 1);
+        }
     }
 }
 
