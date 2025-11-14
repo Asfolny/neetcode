327803e4734ba400dd23b5f6e6233487cfcd65e1
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-11-14 22:52:52 +0100

    JAVA: OPTIMIZE solution for Subsets
    This is better because it counts down, instead of summing up
    
    Also skips iteration over dfs with index +1

--- combination-sum/solution.java
+++ combination-sum/solution.java
@@ -1,29 +1,26 @@
 class Solution {
+    List<List<Integer>> res;
     public List<List<Integer>> combinationSum(int[] nums, int target) {
-        var sets = new HashSet<List<Integer>>();
+        res = new ArrayList<List<Integer>>();
         var set = new ArrayList<Integer>();
-        dfs(nums, 0, target, set, sets);
-        var res = new ArrayList<List<Integer>>();
-        for (var subset : sets) {
-            res.add(subset);
-        }
+        dfs(nums, 0, target, set);
         return res;
     }
 
-    private void dfs(int[] nums, int index, int target, List<Integer> set, Set<List<Integer>> res) {
-        var total = set.stream().reduce(0, Integer::sum);
-        if (total >= target || index >= nums.length) {
-            if (total == target) {
-                res.add(new ArrayList<>(set));
-            }
+    private void dfs(int[] nums, int index, int target, List<Integer> cur) {
+        if (target == 0) {
+            res.add(new ArrayList(cur));
             return;
         }
 
-        set.add(nums[index]);
-        dfs(nums, index, target, set, res);
-        dfs(nums, index+1, target, set, res);
-        set.remove(set.size()-1);
-        dfs(nums, index+1, target, set, res);
+        if (target < 0 || index >= nums.length) {
+            return;
+        }
+
+        cur.add(nums[index]);
+        dfs(nums, index, target - nums[index], cur);
+        cur.remove(cur.size()-1);
+        dfs(nums, index+1, target, cur);
     }
 }
 
